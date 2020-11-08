2f95b157ee1ca8fe376d8836eacc56309de9147d
pshepelenko <69534434+pshepelenko@users.noreply.github.com>
2020-11-08 20:20:09 +0300

    reset changes because it stopper working locally

--- src/views/starter/starter.jsx
+++ src/views/starter/starter.jsx
@@ -10,11 +10,11 @@ import {
     Row,
     Col
 } from 'reactstrap';
-import { SalesSummary, Projects, Feeds } from '../../components/dashboard-components';
+import { SalesSummary, Projects, Feeds } from 'components/dashboard-components';
 
-import img1 from '../../assets/images/big/img1.jpg';
-import img2 from '../../assets/images/big/img2.jpg';
-import img3 from '../../assets/images/big/img3.jpg';
+import img1 from 'assets/images/big/img1.jpg';
+import img2 from 'assets/images/big/img2.jpg';
+import img3 from 'assets/images/big/img3.jpg';
 
 const Starter = () => {
     return (
